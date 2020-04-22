d09a8b4317b002e25c95cdaa7eea997133953a67
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-04-22 09:11:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+ 
+    get "/" do
+        erb :index
+    end
+
+    get "/new" do
+        erb :new
+    end
+
+    post "/puppy" do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        # @name = params[:name]
+        # @breed = params[:breed]
+        # @age = params[:age]
+        # binding.pry
+        erb :puppy
+    end
 
 end
